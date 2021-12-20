f8acaad2fb6b5a334f446584906d22a15ad1e3e9
Emre KESIM <emrekesim34@gmail.com>
2021-12-20 16:42:45 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import Login from "./pages/Login"
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import './App.css';
+import CopyRight from './components/CopyRight';
+import Register from './pages/Register';
+import Home from "./pages/Home";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <CopyRight />
+      <Routes>
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/home" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
